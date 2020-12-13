a3707a89090c4cdeaec1e27cc9a8f277d5bcd67c
Triapitsyn Ihor <43449370+Igore4ik@users.noreply.github.com>
2020-12-13 18:29:14 +0200

    debugs reducers create copy of state in reducers files to rerender DOM

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./redux/redux-store";
-import { Provider } from "./StoreContext";
+import { Provider } from "react-redux";
 
 const rootElement = document.getElementById("root");
 
-const rerenderEntireTree = (store) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    rootElement
-  );
-};
-rerenderEntireTree(store);
-store.subscribe(() => {
-  rerenderEntireTree(store);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
