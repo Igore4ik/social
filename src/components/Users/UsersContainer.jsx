5ccd578aaceb7f07c5bebeae642eb58ea7088005
Triapitsyn Ihor <43449370+Igore4ik@users.noreply.github.com>
2020-12-15 19:30:58 +0200

    refactoring create Users FComponent

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
   followAC,
@@ -6,6 +7,46 @@ import {
   unFollowAC
 } from "../../redux/reducer-users";
 import Users from "./Users";
+import axios from "axios";
+
+class UsersContainer extends Component {
+  componentDidMount() {
+    // axios.get("https://social-network.samuraijs.com/api/1.0/users")
+    axios
+      .get(
+        `https://randomuser.me/api/?results=${this.props.pageSize}&page=${this.props.currentPage}&seed=0e7291e8b94f7b30`
+      )
+
+      .then((response) => {
+        this.props.setUsers(response.data.results);
+      });
+  }
+  setPage = (n) => {
+    this.props.setCerrentPage(n);
+    axios
+      .get(
+        `https://randomuser.me/api/?results=${this.props.pageSize}&page=${n}&seed=0e7291e8b94f7b30`
+      )
+
+      .then((response) => {
+        this.props.setUsers(response.data.results);
+      });
+  };
+
+  render() {
+    return (
+      <Users
+        users={this.props.users}
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        setPage={this.setPage}
+        currentPage={this.props.currentPage}
+        followUser={this.props.followUser}
+        unFollowUser={this.props.unFollowUser}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -32,6 +73,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
